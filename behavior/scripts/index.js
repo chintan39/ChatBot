c4b80fd2c73d11148de8e9baf4b88aa975eaa18d
Chintan Parikh <hi@chintan.pw>
2017-01-24 00:25:10 +0530

    Added first weather conversation

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,49 +1,75 @@
-'use strict'
+/*jshint esversion:6 */
 
 exports.handle = (client) => {
   // Create steps
   const sayHello = client.createStep({
     satisfied() {
-      return Boolean(client.getConversationState().helloSent)
+      return Boolean(client.getConversationState().helloSent);
     },
 
     prompt() {
-      client.addResponse('welcome')
+      client.addResponse('welcome');
       client.addResponse('provide/documentation', {
         documentation_link: 'http://docs.init.ai',
-      })
-      client.addResponse('provide/instructions')
+      });
+      client.addResponse('provide/instructions');
 
       client.updateConversationState({
         helloSent: true
-      })
+      });
 
-      client.done()
+      client.done();
     }
-  })
+  });
 
   const untrained = client.createStep({
     satisfied() {
-      return false
+      return false;
     },
 
     prompt() {
-      client.addResponse('apology/untrained')
-      client.done()
+      client.addResponse('apology/untrained');
+      client.done();
     }
-  })
+  });
+
+  const handleGreeting = client.createStep({
+      satisfied() {
+        return false;
+      },
+
+      prompt() {
+        client.addTextResponse('Hello world, I mean human');
+        client.done();
+      }
+  });
+
+  const handleGoodbye = client.createStep({
+      satisfied() {
+        return false;
+      },
+
+      prompt() {
+        client.addTextResponse('See you later!');
+        client.done();
+      }
+  });
 
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
+      goodbye: 'goodbye',
+      greeting: 'greeting'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained],
     },
-  })
-}
+  });
+};
